5061356ff67cbae59564e21b230a2bbf39694d22
oliveiraallex <allex.oliveira@yahoo.com.br>
2023-10-18 16:32:27 -0400

    finish lab03

--- dsec/lab03/script10-knock.sh
+++ dsec/lab03/script10-knock.sh
@@ -7,13 +7,6 @@ hping3 -S -p 37 -c 1 $ip
 hping3 -S -p 30000 -c 1 $ip
 hping3 -S -p 3000 -c 1 $ip
 hping3 -S -p 1337 -c 1 $ip
-
-nmap -sV $ip
-
-#hping3 -S -p 21 -c 1 $ip 
-#hping3 -S -p 22 -c 1 $ip 
-#hping3 -S -p 23 -c 1 $ip 
-#hping3 -S -p 80 -c 1 $ip 
-#hping3 -S -p 443 -c 1 $ip 
-#hping3 -S -p 1337 -c 1 $ip
+nmap -Pn -sV $ip -p 1337
+{ echo "GET / HTTP/1.0"; echo ""; sleep 1; } | telnet $ip 1337
 
